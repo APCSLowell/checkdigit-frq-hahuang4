37d57acd2e83249fb9963a521e708d41b21d3ea6
hahuang4 <142834501+hahuang4@users.noreply.github.com>
2024-04-29 22:57:34 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -11,7 +11,7 @@ public class CheckDigit
      int sum=0;
      for(int i=1;i<= getNumberOfDigits(num); i++)
        {
-         sum +=(8-i)* getDight(num, i);
+         sum +=(8-i)* getDigit(num, i);
        }
      return sum%10;
    }
